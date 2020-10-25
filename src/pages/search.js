f417fd7c808bdeb87e24c35bb91a9e5d5a58c683
slavikshakhov <v.shakhov@yahoo.ca>
2020-10-25 08:32:47 +0100

    import of gatsby

--- src/pages/search.js
+++ src/pages/search.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 import Items from "../components/Items/Items"
+import { graphql } from "gatsby"
 
 const SearchPage = ({ data }) => {
   return (
